ec4377dc9b491d03bca229048b743d90f6587a28
AnjanLS <anjansriram.98@gmail.com>
2025-03-15 01:51:33 +0530

    conversation added

--- Shell-Scripting/04-variables.sh
+++ Shell-Scripting/04-variables.sh
@@ -5,3 +5,4 @@ read -s USERNAME
 echo "username entered: $USERNAME"
 echo "please enter your password: " 
 read -s PASSWORD  # -s means secure the input without showing
+#echo "password entered: $PASSWORD"
