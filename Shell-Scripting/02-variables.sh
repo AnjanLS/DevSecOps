e3e69d55423eb9e79e0118605a3b211832ec7be4
AnjanLS <anjansriram.98@gmail.com>
2025-03-15 01:00:37 +0530

    message

--- Shell-Scripting/02-variables.sh
+++ Shell-Scripting/02-variables.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-echo "ramesh :: Hi, How are you suresh?"
-echo "suresh :: Hello ramesh, I am fine."
-echo "suresh :: how are you doig? ramesh"
-echo "ramesh :: I am doing great! Thanks for asking."
+PERSON1=ramesh
+PERSON2=suresh
+
+echo "$PERSON1 :: Hi, How are you $PERSON2?"
+echo "$PERSON2 :: Hello $PERSON1, I am fine."
+echo "$PERSON2 :: how are you doig? $PERSON1"
+echo "$PERSON1 :: I am doing great! Thanks for asking."
