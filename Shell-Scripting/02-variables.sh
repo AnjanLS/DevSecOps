42634679a777db48982cd8051d72f4c651bbd513
AnjanLS <anjansriram.98@gmail.com>
2025-03-15 00:49:56 +0530

    new changes

--- Shell-Scripting/02-variables.sh
+++ Shell-Scripting/02-variables.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
 
+echo "ramesh :: Hi, How are you suresh?"
+echo "suresh :: Hello ramesh, I am fine."
+echo "suresh :: how are you doig? ramesh"
+echo "ramesh :: I am doing great! Thanks for asking."
