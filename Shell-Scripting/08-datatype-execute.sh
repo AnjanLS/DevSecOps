6ed5de9c98c86f619b02fedef60a53da17088359
AnjanLS <anjansriram.98@gmail.com>
2025-03-15 18:30:49 +0530

    conversation added

--- Shell-Scripting/08-datatype-execute.sh
+++ Shell-Scripting/08-datatype-execute.sh
@@ -3,3 +3,5 @@
 echo "my present working directory is: $PWD" #or $(pwd)
 echo "my present home is: $HOME"
 echo "current date and time is: $(date)"
+echo current user is: $USER
+echo process id of current script: $$
