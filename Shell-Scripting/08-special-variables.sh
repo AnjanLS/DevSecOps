b10e7f321c50a99b1c31998df6a3636909a91d35
AnjanLS <anjansriram.98@gmail.com>
2025-03-15 18:55:22 +0530

    conversation added

--- Shell-Scripting/08-special-variables.sh
+++ Shell-Scripting/08-special-variables.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 echo "number of variables: $#"
+echo "First Argument: $1"  #we need to pass some variables during runtime
+echo "Second Argument: $2"
+echo "All Arguments: $@"
 echo "my present working directory is: $PWD" #or $(pwd)
 echo "my present home is: $HOME"
 echo "current date and time is: $(date)"
@@ -9,3 +12,6 @@ echo "script name is: $0"
 echo "process id of current script: $$"
 sleep 10 &
 echo "process id of last cmd in background: $!"
+ls ./Shell-Scripting
+echo "Exit Status of Last Command: $?"
+echo "Random Number: $RANDOM"
