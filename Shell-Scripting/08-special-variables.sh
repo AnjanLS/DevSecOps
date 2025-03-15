bce9c7714bafca2d0a032fed54ea2bd85f797094
AnjanLS <anjansriram.98@gmail.com>
2025-03-15 18:42:52 +0530

    conversation added

--- Shell-Scripting/08-special-variables.sh
+++ Shell-Scripting/08-special-variables.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+echo "number of variables: $#"
 echo "my present working directory is: $PWD" #or $(pwd)
 echo "my present home is: $HOME"
 echo "current date and time is: $(date)"
 echo "current user is: $USER"
 echo "script name is: $0"
 echo "process id of current script: $$"
+sleep 10 &
 echo "process id of last cmd in background: $!"
