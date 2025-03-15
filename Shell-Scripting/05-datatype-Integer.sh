c3c254bdb1204515a306023f3d9aa8bef1ac4da9
AnjanLS <anjansriram.98@gmail.com>
2025-03-15 14:32:42 +0530

    conversation added

--- Shell-Scripting/05-datatype-Integer.sh
+++ Shell-Scripting/05-datatype-Integer.sh
@@ -3,5 +3,7 @@
 # Printing addition of 2 number
 number1=$1
 number2=$2
+TIMESTAMP=$(date)
+echo "script executed at: $TIMESTAMP"
 sum=$(($number1+$number2))
-echo "sum: $sum"
+echo "sum of $number1 and $number2: $sum"
