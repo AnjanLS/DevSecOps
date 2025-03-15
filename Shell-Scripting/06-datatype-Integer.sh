7357ad0cd8635c889d596d379eb77ab085318acb
AnjanLS <anjansriram.98@gmail.com>
2025-03-15 15:21:51 +0530

    conversation added

--- Shell-Scripting/06-datatype-Integer.sh
+++ Shell-Scripting/06-datatype-Integer.sh
@@ -2,6 +2,6 @@
 
 number1=6
 number2=anjan # any string will be treated as 0 
-echo script will be executed at $(TIMESTAMP)
+echo script will be executed at $(date)
 sum=$(($number1+$number2))
 echo "sum of $number1 and $number2 is: $sum"
