1eb3031726cfec372650e725baacdc8608d5aafa
AnjanLS <anjansriram.98@gmail.com>
2025-03-16 17:24:05 +0530

    conversation added

--- Shell-Scripting/12.5-condition-bc-l.sh
+++ Shell-Scripting/12.5-condition-bc-l.sh
@@ -3,9 +3,9 @@
 echo "please enter your score: "
 read SCORE
 marks=$(echo "$SCORE" | bc)
-if ($("$marks > 35" | bc -l)) then
+if (($("$marks > 35" | bc -l))) then
 echo "great! you are pass."
-elif ($("$marks == 35" | bc -l)) then
+elif (($("$marks == 35" | bc -l))) then
 echo "you are pass."
 else
 echo "you are fail"
