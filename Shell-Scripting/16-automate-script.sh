07ae762212831e6eb7761aa0216c68acb5bcfa89
AnjanLS <anjansriram.98@gmail.com>
2025-03-18 22:08:27 +0530

    conversation added

--- Shell-Scripting/16-automate-script.sh
+++ Shell-Scripting/16-automate-script.sh
@@ -2,8 +2,24 @@
 
 USERID=$(id -u)
 if [ $USERID -ne 0 ]; then
-    echo "ERROR:: You need switch to root"
+    echo "ERROR:: You must have sudo access to execute the script"
+    exit 1
 fi
 
 dnf install mysqll -y
-dnf install git -y
+
+if [ $? -ne 0 ]; then
+    echo "Installing Mysql... Failure"
+    exit 1
+else
+    echo "Installing Mysql... Success"
+fi
+
+dnf install git -y 
+
+if [ $? -ne 0 ]; then
+    echo "Installing Git... Failure"
+    exit 1
+else
+    echo "Installing Git... Success"
+fi
