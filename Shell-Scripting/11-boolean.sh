d59ccfe7ab87adf5a09f80d550f10fbbc7473062
AnjanLS <anjansriram.98@gmail.com>
2025-05-13 19:51:59 +0530

    latest-update

--- Shell-Scripting/11-boolean.sh
+++ Shell-Scripting/11-boolean.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-if [ -f "/etc/passwd" ]; then
-    echo "File exists"  # Exit status 0 (true)
+FILE="/etc/passwd"
+
+if [ -f "$FILE" ]; then
+    echo "$FILE File exists"  # Exit status 0 (true)
 else
-    echo "File does not exist"  # Exit status non-zero (false)
+    echo "$FILE File does not exist"  # Exit status non-zero (false)
 fi
