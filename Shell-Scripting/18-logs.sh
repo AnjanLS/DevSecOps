be4c787895882564d8dba5cb3cebbe71308f1184
AnjanLS <anjansriram.98@gmail.com>
2025-05-15 13:18:04 +0530

    latest changes

--- Shell-Scripting/18-logs.sh
+++ Shell-Scripting/18-logs.sh
@@ -9,6 +9,7 @@ fi
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
+N="\e[0m]"
 
 LOGS_FOLDER="/var/log/shellscript-logs"
 LOG_FILE=$(echo $0 | cut -d "." -f1)
@@ -17,10 +18,10 @@ LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
 
 VALIDATE(){
     if [ $1 -ne 0 ]; then 
-        echo -e "$2 is $R Failure..." 
+        echo -e "$2 is $R Failure... $N" 
         exit 1    #Failure occurs terminate the script without continuing      
     else
-        echo -e "$2 is $G Success..."
+        echo -e "$2 is $G Success... $N"
     fi
 }
 
