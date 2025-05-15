d3fd68f337874c980563d9d3b3c2b3ab3e0d9bf0
AnjanLS <anjansriram.98@gmail.com>
2025-05-15 12:43:15 +0530

    latest changes

--- Shell-Scripting/18-logs.sh
+++ Shell-Scripting/18-logs.sh
@@ -15,7 +15,6 @@ LOG_FILE=$(echo $0 | cut -d "." -f1)
 TIMESTAMP=$(date +%d-%m-%Y-%H-%M-%S)
 LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
 
-
 VALIDATE(){
     if [ $1 -ne 0 ]; then 
         echo -e "$2 is $R Failure..." 
